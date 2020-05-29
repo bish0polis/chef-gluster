330b1f1000feb6a2ccb005eeec4515d10430508c
Matthieu Serrepuy <lotooo@gmail.com>
2020-05-29 14:37:55 +0200

    bump version to 6.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'shortdudey123@gmail.com'
 license          'Apache-2.0'
 description      'Installs and configures Gluster servers and clients'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '6.2.1'
+version          '6.2.2'
 depends          'lvm', '>= 1.5.1'
 
 source_url 'https://github.com/shortdudey123/chef-gluster'
